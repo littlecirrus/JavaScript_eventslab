0f2bdf908927d517281e57104a3f60c2eea3d70d
sarahmurphy86 <sarah.graham86@gmail.com>
2018-06-21 16:33:22 +0100

    lab work pushed up

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
-})
+
+const form = document.querySelector('#new-item-form');
+const container = document.querySelector('.container');
+
+
+const handleFormInput = function(event){
+  event.preventDefault();
+  const buttonResult = document.querySelector(`#button-result`);
+   const resultString = `You typed: ${this.author.value} ${this.title.value} ${this.category.value}`;
+   createParagraph(resultString, buttonResult);
+
+  form.reset();
+};
+
+form.addEventListener('submit',handleFormInput);
+
+const createParagraph = function(content, container){
+  const paragraph = document.createElement('p');
+  paragraph.textContent = content;
+  container.appendChild(paragraph);
+};
+
+});
